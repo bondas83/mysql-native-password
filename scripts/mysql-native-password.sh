a47be2fce31a506c28bbca3c6ed844d9f095916a
Julius Šmatavičius <julius.smatavicius@lotelita.lt>
2019-02-08 15:13:46 +0200

    fix command line

--- scripts/mysql-native-password.sh
+++ scripts/mysql-native-password.sh
@@ -16,4 +16,4 @@
 
 # The mysql-init-complete file is touched by the entrypoint file before the
 # main server process is started
-RUN sed -i '/^\[mysqld\]$/a\default-authentication-plugin=mysql_native_password' /etc/mysql/my.cnf
+sed -i '/^\[mysqld\]$/a\default-authentication-plugin=mysql_native_password' /etc/mysql/my.cnf
